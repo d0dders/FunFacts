940dad2c3c0752a67364a8376266801576b7e65b
d0dders <dodders@outlook.com>
2018-12-03 20:34:03 +0000

    Maintain fact when rotating screen (save instance state)

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -1,25 +1,46 @@
 package com.teamtreehouse.funfacts;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class FunFactsActivity extends AppCompatActivity {
     public static final String TAG = FunFactsActivity.class.getSimpleName();
+    private static final String KEY_FACT = "KEY_FACT";
+    private static final String KEY_COLOUR = "KEY_COLOUR";
     private FactBook factBook = new FactBook();
     private ColorWheel colorWheel = new ColorWheel();
     // Declare our View variables
     private TextView factTextView;
     private Button showFactButton;
     private RelativeLayout relativeLayout;
+    private String mFact;
+    private int mColor;
 
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        mFact = savedInstanceState.getString(KEY_FACT);
+        factTextView.setText(mFact);
+        mColor = savedInstanceState.getInt(KEY_COLOUR);
+        relativeLayout.setBackgroundColor(mColor);
+        showFactButton.setTextColor(mColor);
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        
+        outState.putString(KEY_FACT, mFact);
+        outState.putInt(KEY_COLOUR, mColor);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +69,10 @@ public class FunFactsActivity extends AppCompatActivity {
     }
 
     private void displayNewFact() {
-        int color = colorWheel.getColor();
-        relativeLayout.setBackgroundColor(color);
-        factTextView.setText(factBook.getFact());
-        showFactButton.setTextColor(color);
+        mColor = colorWheel.getColor();
+        relativeLayout.setBackgroundColor(mColor);
+        showFactButton.setTextColor(mColor);
+        mFact = factBook.getFact();
+        factTextView.setText(mFact);
     }
 }
